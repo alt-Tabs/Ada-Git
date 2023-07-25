6ea80c1cf26a18730f598404ef1db7b6ac019d8d
Tabs <devtabs2019@gmail.com>
2023-07-25 20:11:58 -0300

    aulas 4-5 poo

--- POO/objetos/src/main/java/Animais/Cachorro.java
+++ POO/objetos/src/main/java/Animais/Cachorro.java
@@ -1,8 +1,132 @@
 package Animais;
 
 public class Cachorro {
-    public String nome;
-    public String cor;
-    public double altura;
-    public int peso;    
+    private String nome;
+    private String cor;
+    private double altura;
+    private int peso;
+    private boolean akumaNoMi;
+    private String estado;
+
+    public Cachorro(String nome, String cor, double altura, int peso, boolean akumaNoMi, String estado) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.akumaNoMi = akumaNoMi;
+        this.estado = estado;
+    }
+    public Cachorro(){}; 
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public void comer(){}
+    
+    public void latir(){
+        System.out.println("Mestre ODENNN");
+    }
+    
+    public String pegar(){
+        return "Espada";
+    }
+    
+    public boolean possuiAkumaNoMi (String acao){
+        if(acao.equals("sim")){
+            this.setAkumaNoMi(true);
+        }
+        return isAkumaNoMi();
+    }
+    
+    public String atacar(String origemInimigo){
+        switch(origemInimigo){
+            case "East Blue": this.setEstado("Espadachim");break;
+            case "Alabasta": this.setEstado("Electro");   break;
+            default: this.setEstado("Atento");break;
+        }return getEstado();
+        
+        
+//        if(origemInimigo.equals("East Blue")){
+//            this.estado = "Espadachim";
+//        } else if (origemInimigo.equals("Alabasta")){
+//            this.estado = "Electro";
+//        }return estado;
+        }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cor
+     */
+    public String getCor() {
+        return cor;
+    }
+
+    /**
+     * @param cor the cor to set
+     */
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    /**
+     * @return the altura
+     */
+    public double getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * @return the peso
+     */
+    public int getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return the akumaNoMi
+     */
+    public boolean isAkumaNoMi() {
+        return akumaNoMi;
+    }
+
+    /**
+     * @param akumaNoMi the akumaNoMi to set
+     */
+    public void setAkumaNoMi(boolean akumaNoMi) {
+        this.akumaNoMi = akumaNoMi;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
--- POO/objetos/src/main/java/Main.java
+++ POO/objetos/src/main/java/Main.java
@@ -4,12 +4,25 @@ import Animais.Cachorro;
 public class Main {
     public static void main(String[] args) {
         Cachorro cachorro1 = new Cachorro();
-        cachorro1.nome = "Inuarashi";
-        cachorro1.cor = "Caramelo";
-        cachorro1.altura = 5.11;
-        cachorro1.peso = 200;
+        Cachorro cachorro2 = new Cachorro("Rex", "Azul",1.70, 8, false, "wano");
         
-        System.out.println(cachorro1);
+        cachorro1.setNome("Inuarashi");
+        cachorro1.setCor("Caramelo");
+        cachorro1.setAltura(1.77);
+        cachorro1.setPeso(200);
+        
+        cachorro2.getNome();
+        cachorro2.getAltura();
+        
+        //System.out.println(cachorro1);
+        cachorro1.latir();
+        System.out.println(cachorro1.pegar());
+        System.out.println("Este usuário comeu alguma fruta do diabo? "
+                +cachorro1.possuiAkumaNoMi("Não"));
+        System.out.println("Inuarashi atacará em modo "+ cachorro1.atacar("Alabasta"));
+        
+        System.out.println(cachorro1.toString());
+        System.out.println(cachorro2.toString());
     }
     
 }
